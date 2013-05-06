c7ca40211970c4a8068c256e50acb442220ee949
osm2xp <osm2xp@gmail.com>
2013-05-06 06:13:13 +0000

    X-plane smart exclusions

---
+++ osm2xp/src/com/osm2xp/utils/helpers/XplaneExclusionsHelper.java
@@ -0,0 +1,15 @@
+package com.osm2xp.utils.helpers;
+
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+
+import com.osm2xp.model.osm.OsmPolygon;
+
+public class XplaneExclusionsHelper {
+
+	private List<Rectangle2D> exclusions;
+
+	public void addBuildingToExclusions(OsmPolygon osmPolygon) {
+
+	}
+}
